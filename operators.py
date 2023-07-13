29fdea9db0a23a1ec50a8ec647463775394cf670
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-13 23:17:59 +0530

    Python Arithmatic Operator Usage

---
+++ operators.py
@@ -0,0 +1,22 @@
+print (6+2) 
+
+print (6-2) 
+
+print (1000*3) 
+
+print (1000/5) 
+
+
+print (6 // 5)  # This will return 1  because 1.2 is near to 1
+
+print (6 % 5) # This will give 1  because modules division get the remainder 
+
+print (2 ** 3 ) # This will give 8
+
+print ( -6 - 6)   # This will give -12 
+
+
+
+
+
+
