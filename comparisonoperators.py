7dd8f01ded7d4db027860b96e95a455ac36b2b1e
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-15 22:41:22 +0530

    Comparison Operator Usage

---
+++ comparisonoperators.py
@@ -0,0 +1,12 @@
+#This will print true or false depending on the comparison operator
+print ( 2 == 2 )
+
+print ( 2 != 2)
+
+print ( 3 > 5)
+
+print (3 >= 2 )
+
+print ( 5 < 8 )
+
+print ( 50 <= 50)
