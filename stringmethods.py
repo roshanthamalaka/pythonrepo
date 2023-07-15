a4eaa83fe969558dcfa109d5b57bd4a4f65c972c
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-15 22:27:23 +0530

    String method usage

---
+++ stringmethods.py
@@ -0,0 +1,14 @@
+#Concatante strings  with + Operator
+
+print("Mahinda"+" "+"hora")
+
+# Repeate same string multipe times using * Operator
+
+print(" Ranil Ponnaya "*5)
+
+number = 5 
+
+total = number + 5 
+
+# Use Str for for type casting which means convert integer to string
+print("Total is "+ str(total))
