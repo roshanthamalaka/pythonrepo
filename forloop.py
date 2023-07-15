dc4404d626f3a12d8662bdd47d4e75e1f3ce591a
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-16 00:43:42 +0530

    For Loop Usage

---
+++ forloop.py
@@ -0,0 +1,20 @@
+i=1 
+
+for i in range (5):
+    print ("i is ",i)
+
+j =1 
+
+for j in range(10):
+    if (j == 5 ):
+        break
+    print ("j is ",j)
+
+
+q = 1
+
+for q in range (20):
+    if (q ==10 ): 
+        continue
+    print ("Q is ",q)
+
