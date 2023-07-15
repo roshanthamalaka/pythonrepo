36d916a0c831d5e20424802ae9a9a0ed12707968
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-15 23:00:33 +0530

    Conditions usage example

---
+++ conditions.py
@@ -0,0 +1,10 @@
+party=input ("Enter Your Political Party ")
+
+if party == "SLPP":
+    print ("Your are Baiyya")
+elif party == "UNP" or party== "SJB":
+    print ("You are Lolla")
+elif party == "NPP" or party== "JVP":
+    print ("You are Jeppek")
+else: 
+    print ("Party Not Found")
