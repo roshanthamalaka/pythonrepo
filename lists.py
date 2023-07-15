3184149832546fb6f906bf9dce010b63533a9b8b
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-16 01:44:00 +0530

    Basic of list data type usage in python

---
+++ lists.py
@@ -0,0 +1,16 @@
+numbers = [10,20,30,40,50]
+
+print (numbers[2]) 
+
+print (numbers[-5]) # Use of Negative index in python
+
+total=len(numbers) 
+print ("Total is ",total)
+
+
+del(numbers[3]) # In this case 40 will be deleted. Array element count will set to 4 so now 50 index is 3
+
+print ("Deleting an element")
+
+total=len(numbers) 
+print ("Total is ",total)
