721e74eaa92143e7a9fe7b41b865404ddf1e566e
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-16 23:07:52 +0530

    List data type manipulation methods

---
+++ list_methods.py
@@ -0,0 +1,30 @@
+# Code contains related to the  manipulation of list
+
+nums = [10,20,40]
+
+print ("Intial List",nums)
+
+#variable_name.insert(index,value)
+
+nums.insert(2,30)
+
+print("List After Insert methods usage",nums)
+
+nums.append(50)
+
+print("List After Append",nums)
+
+
+print("Sort and Append Usage Testing")
+
+snms = [80,30,100,60,20]
+
+print("Initial List Before Sorting",snms)
+
+snms.sort()
+
+print("List After sorting",snms)
+
+snms.reverse()
+
+print("List After Reverse",snms)
