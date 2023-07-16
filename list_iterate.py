78f3c539af287f0febc76fdc689410f2f87c687a
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-16 23:22:14 +0530

    Iterate throug list using for loop example

---
+++ list_iterate.py
@@ -0,0 +1,15 @@
+num = [10,20,30,45,60]
+
+sum=0
+for i in num: 
+    print (i)
+    sum = sum+i
+
+
+print("Total of the list is ",sum)
+
+lenth = len(num)
+
+average = sum/lenth
+
+print("Average of the list is ",average)
