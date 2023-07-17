b2a532cb37989e1234db8fa26da9129272174ec3
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-17 21:39:13 +0530

    2D List Usage

---
+++ 2dlist.py
@@ -0,0 +1,10 @@
+names = [
+["Mahinda","Basil","Namal"], #0
+["Ranil","Akila","Sagala"],  #1 
+["Sajith","Harsha","Pakaya"] #2
+  #0         1         2
+]
+
+hora = names[2][1] # This value is harsha
+
+print("Gona is ",hora)
