ab99c9cfa5bf2f5b3b12ed9ba20c41ae6c0ec419
roshanthamalaka <roshanthamalaka@gmail.com>
2023-07-18 18:06:22 +0530

    Tuple Data type usage example

---
+++ tuples.py
@@ -0,0 +1,22 @@
+# Tuple Data type usage 
+# Similar to list but We can change the element of the tuple 
+
+tuple1= (1,2,3)
+
+tuple2= 1,2,3
+
+age= 99
+tuple3 = (1,"Hora",age,(7,8)) # Tuple can contain multiple data types
+
+# We can create Tuple with single element
+# But make sure to put , at the end
+
+tuple4= (1,)
+tuple5 = 8,
+
+print("Tuple Three contains multiple values")
+print(tuple3)
+
+print("Tuple 4 Contains single element")
+
+print(tuple4)
